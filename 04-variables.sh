cbb4bcd26eac8cf8e7a938012e89e7aad9918353
sushmaaws-pro <sushmaaws2025@gmail.com>
2025-09-27 20:02:55 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "T$PERSON1:: Welcome tto Alaska $PERSON2"
+echo "$PERSON2:: Thank you $PERSON1, Finally not on Zoom Webiner"
+echo "$PERSON1:: So, doo we have a deal"
+echo "$PERSON2:: Yes, if you make Ukraine joins Russia"
