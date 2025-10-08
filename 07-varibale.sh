703d79e65f2c9579128ba291b20a1fd60e4ab663
sushmaaws-pro <sushmaaws2025@gmail.com>
2025-10-08 18:40:08 +0530

    Prcatice

---
+++ 07-varibale.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#DATE=$(date)
+
+START_TIME=$(date +%s)
+
+sleep 10 &
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+
+echo "Script executed in: $TOTALL_TIME SECONDS"
