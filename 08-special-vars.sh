3f80b3c7f0d5b732082c1b62597e5cead894f0c0
sushmaaws-pro <sushmaaws2025@gmail.com>
2025-10-08 19:05:30 +0530

    practice

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -7,3 +7,5 @@ echo "Current directory: $pwd"
 echo "who is running this: $USER"
 echo "Home directory of user: $HOME"
 echo "PID f this script: $$"
+sleep 50 &
+echo "PID of the last command in background is: $!"
