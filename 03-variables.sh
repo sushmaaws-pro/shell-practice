f124e3e9a2022c72a8863bab68d6134667810495
sushmaaws-pro <sushmaaws2025@gmail.com>
2026-02-15 12:17:39 +0530

    some message

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 PERSON1=John
-PERSON12=Raheem
+PERSON2=Raheem
 
 echo "$PERSON1:: Hi $PERSON2, How are yu?"
-echo "$PERSON12:: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
 echo "$PERSON1:: What are you learning recently"
 echo "$PERSON2:: I am learning shell scripting $PERSON1"
