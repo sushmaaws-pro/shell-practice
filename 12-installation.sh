9430a0cd1e02305cfd195cd695ca4f6bfb017e82
sushmaaws-pro <sushmaaws2025@gmail.com>
2026-03-10 11:51:55 +0530

    conditions

--- 12-installation.sh
+++ 12-installation.sh
@@ -6,7 +6,7 @@ if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root privelege"
 fi
 
-dnf insatll mysql -y
+dnf install  mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR:: Intallating MYSQL is failure"
