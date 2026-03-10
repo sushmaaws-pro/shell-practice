2092f6fbcef2bbabb8190d94755040810c31ecd9
sushmaaws-pro <sushmaaws2025@gmail.com>
2026-03-10 11:37:25 +0530

    conditions

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -3,8 +3,8 @@
 echo "Please enter the number"
 read NUMBER
 
-if [ $(($NUMBER % 2)) -wq 0 ]; then
+if [ $(($NUMBER % 2)) -eq 0 ]; then
     echo "Given number $NUMBER is EVEn"
 else 
     echo "Given number $NUMBER is ODD"
-fi        
+fi       
