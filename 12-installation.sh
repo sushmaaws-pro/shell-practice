4fed52f541bf36c5c438ff3440153a967dd21361
sushmaaws-pro <sushmaaws2025@gmail.com>
2026-03-10 11:47:50 +0530

    conditions

---
+++ 12-installation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+USERID=$(ID -U)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+fi
+
+dnf insatll mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: Intallating MYSQL is failure"
+ else
+    echo "Intalling MYSQL is SUCCESS"   
