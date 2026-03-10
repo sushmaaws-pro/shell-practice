ccbf740e9ffad742fea28639d3e6a52d9ab8ae6e
sushmaaws-pro <sushmaaws2025@gmail.com>
2026-03-10 13:06:28 +0530

    conditions

---
+++ 13-functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure other than 0
+fi
+
+VALIDATE(){ #functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo "ERROR:: Installing $2 is failure"
+        exit 1
+    else
+        echo "Installing $2 is SUCCESS"   
+}
+
+
+dnf install mysql -y
+VALIDATE $? "MySQL"
+
+dnf install Nginx -y
+VALIDATE $? "Ngins"
+
+dnf install Python3 -y
+VALIDATE $? "Python3"
---
+++ 15-colors.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m]"
+Y="\e[33m"
+N="\e[0m]"
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "Installing $2...$R is Failure $N"
+        exit 1
+    else
+        echo -e "Intalling $2...$G is SUCCESS"   
+    fi    
+}
+
+
+dnf list installed mysql
+# Intall if not found
+if [ $? -ne 0 ]; then 
+    dnf install mysql -y 
+    VALIDATE $? "MySQL"
+else
+    echo -e "MySql already exit ...$Y SKIPPING $N"
+fi        
+
+dnf list installed Nginx
+if [ $? -ne 0 ]; then
+    dnf install Nginx -y
+    VALIDATE $? "Nginx"
+else
+    echo -e "Nginx already exit ...$Y SKIPPING $N"
+fi
+
+dnf list installed Python3 -y
+if[ $? -ne 0 ]; then
+    dnf install Python3 -y
+    VALIDATE $?  "Python3"
+else
+    echo -e "Python3 already exit ...$Y SKIPPING $N"
+fi    
