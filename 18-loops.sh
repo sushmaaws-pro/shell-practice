e9907d74da16986f067e30b0f99c54ad74efde5c
sushmaaws-pro <sushmaaws2025@gmail.com>
2026-03-14 14:51:27 +0530

    Loops

--- 18-loops.sh
+++ 18-loops.sh
@@ -21,7 +21,7 @@ fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e "Installing  $2 ...$R FAILURE $N" | tee -a $LOGS_FLODER
+        echo -e "Installing  $2 ...$R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     else
         echo -e "Installing $2 ...$G SUCCESS $N" |tee -a $LOG_FILE
