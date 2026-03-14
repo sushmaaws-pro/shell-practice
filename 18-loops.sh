9b76eccab8742b77e072d2f950f6a6271441bd88
sushmaaws-pro <sushmaaws2025@gmail.com>
2026-03-14 15:09:08 +0530

    Loops

--- 18-loops.sh
+++ 18-loops.sh
@@ -36,7 +36,7 @@ do
 
     # if exit status is 0, already installed. -ne 0 need too install it
     if [ $? -ne 0 ]; then
-        dnf install $package -y $>>$LOG_FILE
+        dnf install $package -y &>>$LOG_FILE
         VALIDATE $? "$package"
     else
         echo -e "$package already installed ...$Y SKIPPING $N"
