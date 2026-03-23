328ac53d6db69bc29ca82ebfcd0dea236f4b06cd
sushmaaws-pro <sushmaaws2025@gmail.com>
2026-03-23 11:29:51 +0530

    disk-usage

--- 25-disk-usage.sh
+++ 25-disk-usage.sh
@@ -16,7 +16,7 @@ done <<< $DISK_USAGE
 
 echo -e "Message Body: $MESSAGE"
 
-sh mail.sh "sushmaaws2025@gmail" "High Disk Usage Alert" "High Disk Usage" "$MESSAGE" "$IP_ADDRESS" "DevOps Team"
+sh mail.sh "sushmaaws2025@gmail.com" "High Disk Usage Alert" "High Disk Usage" "$MESSAGE" "$IP_ADDRESS" "DevOps Team"
 
 # TO_ADDRESS=$1
 # SUBJECT=$2
